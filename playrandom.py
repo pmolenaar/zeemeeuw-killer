7fda0f33b1c9e9a1d9bbf1cd698bee85de398501
fronque <ich@racker.fritz.box>
2015-02-11 13:38:42 +0100

    First version

---
+++ playrandom.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os, random, time, sys, select
+
+
+def rndmp3 ():   
+
+  while 1:
+	
+    	randomfile = random.choice(os.listdir("/home/pi/music"))
+	print "Playing file" ,randomfile,"..."
+    	file = ' /home/pi/music/'+ randomfile
+    	os.system ('omxplayer -o local' + file)
+	
+	pause = random.randint(min,max)
+	
+	print "Hit Key and press Enter to stop. Waiting for", pause, "seconds"
+	i, o, u = select.select( [sys.stdin], [], [], pause )
+	if (i):
+		print "STOPPED", sys.stdin.readline().strip()
+		break
+	else:
+		print "next"
+
+os.system("clear") # Clear Screen
+
+# defining waiting time range
+min = 0
+max = 35
+
+print "Audio Random Player Runnning. Waiting Time from ",min,"to",max, "seconds."
+
+rndmp3 ()
+
