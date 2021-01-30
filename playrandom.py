df9bc9b1eded8e5ec17eb84d773927dabf4eae3c
pmolenaar <ppw.molenaar@gmail.com>
2021-01-30 21:40:33 +0100

    Update playrandom.py

--- playrandom.py
+++ playrandom.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
-import os, random, sys, select
+import os, random, sys, select, time
+import gpiozero
 
+# change this value based on which GPIO port the relay is connected to
+RELAY_PIN = 18
+
+# create a relay object. (https://gist.github.com/johnwargo/ea5edc8516b24e0658784ae116628277#file-relay-test-py)
+# Triggered by the output pin going low: active_high=False.
+# Initially off: initial_value=False
+relay = gpiozero.OutputDevice(RELAY_PIN, active_high=False, initial_value=False)
+
+def set_relay(status):
+    if status:
+        print("Setting relay: ON")
+        relay.on()
+    else:
+        print("Setting relay: OFF")
+        relay.off()
+
+def toggle_relay():
+    print("toggling relay")
+    relay.toggle()
+
+	
 
 def rndmp3 ():   
 
